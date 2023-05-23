7ae78369c42f745be1c9acefe62a1b8213b18b6b
AlanDBalan <132862280+AlanDBalan@users.noreply.github.com>
2023-05-23 18:39:12 -0500

    Finished CSS Started JS
    Finished up styling and started on JS
    
    added variables for quizdata

---
+++ assets/script.js
@@ -0,0 +1,37 @@
+var quizData = [
+    {
+        question: "Who was the first President of the United States?";
+        a: "Thomas Jefferson",
+        b: "George Washington",
+        c: "Abraham Lincoln",
+        d: "John F. Kennedy",
+        correct: "b",
+    }
+    {
+        question: "The French Revolution took place in which decade?";
+        a: "1700s",
+        b: "1800s",
+        c: "1900s",
+        d: "2000s",
+        correct: "b",
+    }
+    {
+        question: "Which ancient civilization built the Great Pyramids of Giza?";
+        a: "Mayans",
+        b: "Egyptians",
+        c: "Greeks",
+        d: "Romans",
+        correct: "b",
+    }
+    {
+        question: "The Cold War was primarily a conflict between which two nations?";
+        a: "United States and Soviet Union",
+        b: "United States and China",
+        c: "United States and United Kingdom",
+        d: "United States and Germany",
+        correct: "a",
+    }
+]
+
+var quiz = document.getElementById('quiz')
+var answerEls = document.querySelectorAll('.answer')
