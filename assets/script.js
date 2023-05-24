4e230408eb9d0ebe2fbffb87a304c7d2fad795c7
AlanDBalan <132862280+AlanDBalan@users.noreply.github.com>
2023-05-23 19:30:51 -0500

    Bugfixes
    Bug fixes and cleaned up syntax.

--- assets/script.js
+++ assets/script.js
@@ -33,62 +33,57 @@ var quizData = [
     }
 ];
 
-var quiz= document.getElementById('quiz')
-var answerEls= document.querySelectorAll('.answer')
-var questionEl= document.getElementById('question')
-var a_text= document.getElementById('a_text')
-var b_text= document.getElementById('b_text')
-var c_text= document.getElementById('c_text')
-var d_text= document.getElementById('d_text')
-var submitBtn = document.getElementById('submit')
+var quiz = document.getElementById('quiz');
+var answerEls = document.querySelectorAll('.answer');
+var questionEl = document.getElementById('question');
+var a_text = document.getElementById('a_text');
+var b_text = document.getElementById('b_text');
+var c_text = document.getElementById('c_text');
+var d_text = document.getElementById('d_text');
+var submitBtn = document.getElementById('submit');
 
+let currentQuiz = 0;
+let score = 0;
 
-let curretQuiz = 0
-let score = 0
-
-loadQuiz()
+loadQuiz();
 
 function loadQuiz() {
-
-    deselectAnswers()
-
+    deselectAnswers();
     var currentQuizData = quizData[currentQuiz];
-
-    questionEl.innerText = currentQuizData.question
-    a_text.innerText = currentQuizData.a
-    b_text.innerText = currentQuizData.b
-    c_text.innerText = currentQuizData.c
-    d_text.innerText = currentQuizData.d
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
 }
 
-function deselectAnswers()  {
-    answerEls.forEach(answerEl => answerEl.checked = false)
-
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false);
 }
 
-function getSelected()  {
-    let answer
+function getSelected() {
+    let answer;
     answerEls.forEach(answerEl => {
-        if(answerEl.checked)   {
-            answer = answerEl.id
-    }
-    })
-    return answer
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+    return answer;
 }
 
 submitBtn.addEventListener('click', () => {
-    var answer = getSelected()
-    if(answer)  {
-        if (answer === quizData[currentQuiz].correct)
-            score++
+    var answer = getSelected();
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
     }
 
-    curretQuiz++
+    currentQuiz++;
 
-    if(curretQuiz < quizData.length)    {
-        loadQuiz()
-    }   else {
+    if (currentQuiz < quizData.length) {
+        loadQuiz();
+    } else {
         quiz.innerHTML = `<h2>You answered ${score}/${quizData.length} questions correctly!</h2><button onclick='location.reload()'>Reload</button>`;
     }
-    
-})
+});
