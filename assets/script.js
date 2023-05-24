c92bcff83f5d3f7f6858b78dbb8888968290e43f
AlanDBalan <132862280+AlanDBalan@users.noreply.github.com>
2023-05-23 19:13:37 -0500

    Update script.js
    I did a lot and kind of forgot to commit
    
    added function for selecting answers and quiz results.
    
    Also did some bugfixing

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,37 @@
 var quizData = [
     {
-        question: "Who was the first President of the United States?";
+        question: "Who was the first President of the United States?",
         a: "Thomas Jefferson",
         b: "George Washington",
         c: "Abraham Lincoln",
         d: "John F. Kennedy",
         correct: "b",
-    }
+    },
     {
-        question: "The French Revolution took place in which decade?";
+        question: "The French Revolution took place in which decade?",
         a: "1700s",
         b: "1800s",
         c: "1900s",
         d: "2000s",
         correct: "b",
-    }
+    },
     {
-        question: "Which ancient civilization built the Great Pyramids of Giza?";
+        question: "Which ancient civilization built the Great Pyramids of Giza?",
         a: "Mayans",
         b: "Egyptians",
         c: "Greeks",
         d: "Romans",
         correct: "b",
-    }
+    },
     {
-        question: "The Cold War was primarily a conflict between which two nations?";
+        question: "The Cold War was primarily a conflict between which two nations?",
         a: "United States and Soviet Union",
         b: "United States and China",
         c: "United States and United Kingdom",
         d: "United States and Germany",
         correct: "a",
     }
-]
+];
 
 var quiz= document.getElementById('quiz')
 var answerEls= document.querySelectorAll('.answer')
@@ -52,11 +52,43 @@ function loadQuiz() {
 
     deselectAnswers()
 
-    var currentQuizData = quizData[currentQuizData]
+    var currentQuizData = quizData[currentQuiz];
 
-    questionEl.inneerText = currentQuizData.question
+    questionEl.innerText = currentQuizData.question
     a_text.innerText = currentQuizData.a
     b_text.innerText = currentQuizData.b
     c_text.innerText = currentQuizData.c
     d_text.innerText = currentQuizData.d
 }
+
+function deselectAnswers()  {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+
+}
+
+function getSelected()  {
+    let answer
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked)   {
+            answer = answerEl.id
+    }
+    })
+    return answer
+}
+
+submitBtn.addEventListener('click', () => {
+    var answer = getSelected()
+    if(answer)  {
+        if (answer === quizData[currentQuiz].correct)
+            score++
+    }
+
+    curretQuiz++
+
+    if(curretQuiz < quizData.length)    {
+        loadQuiz()
+    }   else {
+        quiz.innerHTML = `<h2>You answered ${score}/${quizData.length} questions correctly!</h2><button onclick='location.reload()'>Reload</button>`;
+    }
+    
+})
